1335f6c9ef52b158ea4e26a68b7e779c54dcbd26
Pedro Hermes <pedro.hermes2212@gmail.com>
2024-05-28 17:41:53 -0300

    Atualizando

--- Testing Env/Assembly.asm
+++
@@ -1,7 +0,0 @@
-.code
-
-TestingAsm proc
-    mov eax, 666
-    ret
-TestingAsm endp
-    end
--- Testing Env/Main.cc
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-extern "C" int TestingAsm();
-
-int main() {
-    auto ret = TestingAsm();
-    std::cout << "Return value: " << ret << std::endl;
-    return 0;
-}
